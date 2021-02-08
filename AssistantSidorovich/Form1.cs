ce52669a60ae0e795e083c30482af4163eebfcab
Danya Boisan <boberiosif@gmail.com>
2021-02-08 21:54:55 +0200

    Rebuild UI. Added browser search
    Rebuild UI. Added browser search. Ui is doing bara bara bara, bere bere bere, bara bara bara bara bere bere bere

--- AssistantSidorovich/Form1.cs
+++ AssistantSidorovich/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using YTapi;
+using NYoutubeDL;
 
 namespace AssistantSidorovich
 {
@@ -17,24 +18,76 @@ namespace AssistantSidorovich
         {
             InitializeComponent();
         }
+        public string path { get; set; }
+        public string address { get; set; }
+
         YTapi.Search yt = new YTapi.Search();
         private void Form1_Load(object sender, EventArgs e)
         {
            
         }
 
-        private async void button1_Click(object sender, EventArgs e)
+        private void GoogleSearchButton_Click(object sender, EventArgs e)
         {
-            try
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
             {
-                await Task.Run(async () => yt.Start("gachi вечериночка"));
-
-                textBox1.Text = yt.videoKey;
-
+                MessageBox.Show("Searck line is empty!", "Troubles:(", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-            catch
+            else
             {
-                MessageBox.Show("Err");
+                address = $"https://google.com/search?q={textBox1.Text}";
+                System.Diagnostics.Process.Start(address);
+            }
+        }
+
+        private void IconFinderSearchButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Searck line is empty!", "Troubles:(", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                address = $"https://www.iconfinder.com/search?q={textBox1.Text}&price=free";
+                System.Diagnostics.Process.Start(address);
+            }
+        }
+
+        private async void MusicSearchButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Search textBox is empty!");
+            }
+            else
+            {
+                try
+                {
+                    await Task.Run(async () => yt.Start(textBox1.Text));
+                    if (String.IsNullOrWhiteSpace(yt.videoKey))
+                    {
+                        MessageBox.Show("Nothing found!");
+                    }
+                    else
+                    {
+                        textBox1.Text = yt.videoKey;
+                        path = yt.videoTitile + "-" + yt.videoKey + ".mp4";
+                        path = path.Replace("&amp;", "&");
+
+                        var youtubeDl = new YoutubeDL();
+
+                        youtubeDl.YoutubeDlPath = @"..\..\Videos\youtube-dl.exe";
+                        youtubeDl.Options.PostProcessingOptions.ExtractAudio = true;
+                        youtubeDl.Options.VideoFormatOptions.Format = NYoutubeDL.Helpers.Enums.VideoFormat.worst;
+                        MessageBox.Show(path);
+                        MessageBox.Show("Wait, we are dowbloading");
+                        await youtubeDl.DownloadAsync($"https://www.youtube.com/watch?v={yt.videoKey}");
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Err");
+                }
             }
         }
     }
