7436034d2d3a243676b2bda195836ec8069ef929
BulbasDanil <64973521+BulbasDanil@users.noreply.github.com>
2021-02-10 14:48:37 +0200

    Creadet window + classes

---
+++ AssistantSidorovich/Models/Taskk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssistantSidorovich.Models
+{
+    class Taskk
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title}   {Deadline}";
+        }
+    }
+}
--- AssistantSidorovich/TaskManagerForm.cs
+++ AssistantSidorovich/TaskManagerForm.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AssistantSidorovich.Models;
+using System.IO;
+using System.Xml;
 
 namespace AssistantSidorovich
 {
@@ -15,6 +18,59 @@ namespace AssistantSidorovich
         public TaskManagerForm()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Taskk newTask = new Taskk()
+                {
+                    Title = textTitle.Text,
+                    Description = textDesription.Text,
+                    Deadline = dateTimeDeadline.Value
+                };
+
+                listBoxTasks.Items.Add(newTask);
+                listBoxTasks.DisplayMember = newTask.ToString() ;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }    
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFinish_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+            private void textDesription_Click(object sender, EventArgs e)
+        {
+            textDesription.Clear();
+        }
+
+        private void textTitle_Click(object sender, EventArgs e)
+        {
+            textTitle.Clear();
         }
     }
 }
