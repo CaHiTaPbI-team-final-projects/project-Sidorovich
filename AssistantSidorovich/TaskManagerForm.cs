c137c7f4328d9ece174752a087667c783a5642eb
BulbasDanil <64973521+BulbasDanil@users.noreply.github.com>
2021-02-10 15:17:23 +0200

    Xml in motion

--- AssistantSidorovich/TaskManagerForm.cs
+++ AssistantSidorovich/TaskManagerForm.cs
@@ -10,14 +10,23 @@ using System.Windows.Forms;
 using AssistantSidorovich.Models;
 using System.IO;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace AssistantSidorovich
 {
     public partial class TaskManagerForm : Form
     {
+        string path;
+        XDocument xd;
+        XElement root;
+        
         public TaskManagerForm()
         {
             InitializeComponent();
+
+            path = @"..\..\Data\tasks.xml";
+            xd = XDocument.Load(path);
+            root = xd.Element("root");
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
@@ -37,8 +46,11 @@ namespace AssistantSidorovich
             catch (Exception err)
             {
                 MessageBox.Show(err.Message);
-            }    
+            }
 
+            // Edit here
+            xd.Element("root").Add(new XElement("employee", new XAttribute("name", emp.Name), new XAttribute("specialization", emp.Specialization), new XAttribute("weapon", emp.Weapon), new XAttribute("nickname", emp.Nickname),
+                new XAttribute("birth", emp.Birth), new XAttribute("dep_name", emp.Dep_name)));
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
